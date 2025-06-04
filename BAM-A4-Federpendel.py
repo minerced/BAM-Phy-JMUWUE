49b43be34c96a4b421f41e4149a700c3ec6d0b6a
minerced <minerced@gmail.com>
2025-06-04 17:48:07 +0200

    changed a few things when they came up using the script

--- BAM-A4-Federpendel.py
+++ BAM-A4-Federpendel.py
@@ -1,17 +1,16 @@
 import math
-#Eingabe der Werte in Stanadrd!!!! SI einheiten (0 sind placeholder werte)
+#Eingabe der Werte in Stanadard!!!! SI einheiten (0 sind placeholder werte)
     #Literatur Werte
 g=9.80665 #Ortsfaktor g in m*s^-2 Fehlerfrei nach Walcher A1.2
 
     #Eingabe der Geräte Fehler
 o_Waage=10**-4 #Messfehler der Wage in kg
-o_Stoppuhr=0.2 #Messfehler der Stoppuhr in s
+o_Stoppuhr=math.sqrt(2)*0.2 #Messfehler der Stoppuhr in s
 o_Messleiste=10**-3 #Messfehker der Messleiste in m
 
     #Eingabe Messwerte bzw. aus Zeichnung abgelesenen statische Methode
 x_max_stat=0 #Maximale Auslenkungen in m
-m_Halter=0 #Masse des Trägers in kg
-m_Zusatz=(0,0,0,0,0) #Liste mit allen Zusatzmassen für (statisch und dynamisch) in kg (m_ges wird bei Auswertung berechnet)
+m_ges_max=0 #Gasamtmasse bei Maximaler Zeit bzw. Auslenkung
 s_best_stat=0 #Steigung der Best fit Geraden in m/kg
 s_min_stat=0 #Steigung der Minimal Geraden in m/kg
 s_max_stat=0 #Steigung der Maximal Geraden in m/kg
@@ -31,10 +30,9 @@ m_eff_max_zeich=0 #Zeichnerisch bestimmter maximal Wert der effektive Masse
 o_s_best_stat=max(abs(s_best_stat-s_min_stat),(abs(s_best_stat-s_max_stat))) #Fehler der Steigung statische Methode
 D_best_stat_zeich=g/s_best_stat      # BestwertFederkonstante Zeichnerisch bestimmt statische Methode
 o_D_stat_zeich=o_s_best_stat*g/s_best_stat**2 #Fehler Federkonstante Zeichnerisch bestimmt statische Methode
-m_ges=m_Halter+sum(m_Zusatz) #Bestwert Gesamtmasse des Halter und Zusatzmassen statische Methode
-o_m_ges=math.sqrt((1+len(m_Zusatz))*o_Waage**2) #fehler der Gesamtmasse der statischen Methode
-D_stat_rech=m_ges*g/x_max_stat #Bestwert der Federhärte D Rechnersich bestimmt
-o_D_stat_rech=math.sqrt((o_m_ges/m_ges)**2 + (o_Messleiste/x_max_stat)**2)*D_stat_rech #Fehler der Federhärte D rechnerisch bestimmt
+o_m_ges=o_Waage #fehler der Gesamtmasse der statischen Methode
+D_stat_rech=m_ges_max*g/x_max_stat #Bestwert der Federhärte D Rechnersich bestimmt (Fehler da nicht ruhe Lage berücksichtigt)
+o_D_stat_rech=math.sqrt((o_m_ges/m_ges_max)**2 + (o_Messleiste/x_max_stat)**2)*D_stat_rech #Fehler der Federhärte D rechnerisch bestimmt
 
 #Auswertung Dynamische Methode
 T_f=T_n_max/n_mess #ZEit für eine Schwingung
@@ -44,8 +42,8 @@ D_best_dym_zeich=4*(math.pi**2)/s_best_dym #Zeichnerich bestimmte Federhärte na
 D_min_dym_zeich=4*(math.pi**2)/s_min_dym
 D_max_dym_zeich=4*(math.pi**2)/s_max_dym
 o_D_dym_zeich=max(abs(D_best_dym_zeich-D_min_dym_zeich),(abs(D_best_dym_zeich-D_max_dym_zeich)))  #Fehler der Zeichnerisch bestimmten Fehlerhärt nach Dynamischer Methode #bestwert Gesamtmasse des Halter und der Zusatzmassen bei dynamischer Methode
-D_dym_rech=4*(math.pi**2)*m_ges/(T_f**2) #Bestwert der Federhärte Rechnerisch bestimmt für dynamische Methode
-o_D_dym_rech=math.sqrt((o_m_ges/m_ges)**2+4*(o_T_f/T_f)**2)*D_dym_rech #Fehler der Federhärte Rechnerisch bestimmt für dynamische Methode
+D_dym_rech=4*(math.pi**2)*m_ges_max/(T_f**2) #Bestwert der Federhärte Rechnerisch bestimmt für dynamische Methode
+o_D_dym_rech=math.sqrt((o_m_ges/m_ges_max)**2+4*(o_T_f/T_f)**2)*D_dym_rech #Fehler der Federhärte Rechnerisch bestimmt für dynamische Methode
 
 #Bestimmung effektive Masse:
 m_eff_rech=m_Feder/3 #Rechnerisch Bestimmte Effektive Masse

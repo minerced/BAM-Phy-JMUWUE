34d4a920b6f60ac44fe104568420bd2d6e19fa90
minerced <minerced@gmail.com>
2025-06-16 00:55:19 +0200

    finnished BAM5 first draft untested

--- BAM-A5_Zähigkeit_Stokes.py
+++ BAM-A5_Zähigkeit_Stokes.py
@@ -15,6 +15,7 @@ D_Gefaes=0 #Druchmesser des Gefäßes in m
 h_Gefaes=0 #Höhe des Gefäßes in m
 l_mess=0 #Länge der gemessenen Strecke für Zeitmessung in m
 rho_oel=0 #Dichte des Öls in kg/m^3
+o_rho_oel=0 # Fehler für die Dichte des Öls
 
         #erste Kugelsorte
 m_5k_1=0 #Masse für fünf Kugeln der ersten Kugelsorte in kg
@@ -51,7 +52,7 @@ def m_Kugel(m_5,o):
     return (m,o_m)
 
 def t_kugel(t_5):
-    t=t_5/5
+    t=sum(t_5)/5
     h1 = 0
     for i in range(len(t_5)):
         h1 = (0.5 * t_5[i] - t) ** 2 + h1
@@ -65,3 +66,46 @@ def Zähigkeit_Stokes(m,r,rho,t,l, o_m, o_r, o_rho, o_t):
     o_eta=(((o_h1/h1)**2+(o_t/t)**2+(o_Messleiste/l)**2)**0.5)*eta
     return (eta,o_eta)
 
+def Ladenburg (eta0, r):
+    return (eta0/((1+2.1*r/(0.5*D_Gefaes))*(1+3.3*r/h_Gefaes)))
+
+# Radius der vier Kugelsorten in m
+r1, o_r1=r_kugel(d_1)
+r2, o_r2=r_kugel(d_2)
+r3, o_r3=r_kugel(d_3)
+r4, o_r4=r_kugel(d_4)
+
+# Masse der vier Kugelsorten in kg
+m1, o_m1=m_Kugel(m_5k_1, o_Waage)
+m2, o_m2=m_Kugel(m_5k_2, o_Waage)
+m3, o_m3=m_Kugel(m_5k_3, o_Waage)
+m4, o_m4=m_Kugel(m_5k_4, o_Waage)
+
+#Fallzeit der vier Kugelsorten in s
+t1, o_t1=t_kugel(t_5k_1)
+t2, o_t2=t_kugel(t_5k_2)
+t3, o_t3=t_kugel(t_5k_3)
+t4, o_t4=t_kugel(t_5k_4)
+
+#Zähigkeiten der 5 Kugelsorten kg/(s*m)
+eta1, o_eta1=Zähigkeit_Stokes(m1,r1,rho_oel,t1,l_mess, o_m1, o_r1, o_rho_oel, o_t1)
+eta2, o_eta2=Zähigkeit_Stokes(m2,r2,rho_oel,t2,l_mess, o_m2, o_r2, o_rho_oel, o_t2)
+eta3, o_eta3=Zähigkeit_Stokes(m3,r3,rho_oel,t3,l_mess, o_m3, o_r3, o_rho_oel, o_t3)
+eta4, o_eta4=Zähigkeit_Stokes(m4,r4,rho_oel,t4,l_mess, o_m4, o_r4, o_rho_oel, o_t4)
+
+#best-fit gerade mit Zähigkeit auf y-Achse und radius auf x-Achse inklusive fehler
+a, o_a, b, o_b= lr.linear_regression((r1,r2,r3,r4),(eta1,eta2,eta3,eta4),(o_eta1,o_eta2,o_eta3,o_eta4))
+
+#Ladenburg Korrekturen
+eta_Korregiert1=Ladenburg(b,r1)
+eta_Korregiert2=Ladenburg(b,r2)
+eta_Korregiert3=Ladenburg(b,r3)
+eta_Korregiert4=Ladenburg(b,r4)
+
+#Ausgabe
+print("Kugelsorte 1: Radius: ("+str(r1)+"+/-"+str(o_r1)+")m,  Masse: ("+str(m1)+"+/-"+str(o_m1)+")kg,  Fallzeit: ("+str(t1)+'+/-'+str(o_t1)+")s,  Zähigkeit: ("+str(eta1)+"+/-"+str(o_eta1)+")kg/(m*s),   Ladenburg: "+str(eta_Korregiert1)+"kg/(m*s)")
+print("Kugelsorte 2: Radius: ("+str(r2)+"+/-"+str(o_r2)+")m,  Masse: ("+str(m2)+"+/-"+str(o_m2)+")kg,  Fallzeit: ("+str(t2)+'+/-'+str(o_t2)+")s,  Zähigkeit: ("+str(eta2)+"+/-"+str(o_eta2)+")kg/(m*s),   Ladenburg: "+str(eta_Korregiert2)+"kg/(m*s)")
+print("Kugelsorte 3: Radius: ("+str(r3)+"+/-"+str(o_r3)+")m,  Masse: ("+str(m3)+"+/-"+str(o_m3)+")kg,  Fallzeit: ("+str(t3)+'+/-'+str(o_t3)+")s,  Zähigkeit: ("+str(eta3)+"+/-"+str(o_eta3)+")kg/(m*s),   Ladenburg: "+str(eta_Korregiert3)+"kg/(m*s)")
+print("Kugelsorte 4: Radius: ("+str(r4)+"+/-"+str(o_r4)+")m,  Masse: ("+str(m4)+"+/-"+str(o_m4)+")kg,  Fallzeit: ("+str(t4)+'+/-'+str(o_t4)+")s,  Zähigkeit: ("+str(eta4)+"+/-"+str(o_eta4)+")kg/(m*s),   Ladenburg: "+str(eta_Korregiert4)+"kg/(m*s)")
+print()
+print('Lineare-Regression: ('+str(a)+'+/-'+str(o_a)+")*x+("+str(b)+'+/-'+str(o_b)+")")
